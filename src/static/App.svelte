6ccac7b68d177ea963b4c22015bafa57e922e00c
sheodox <imagamer27@msn.com>
2022-06-25 13:24:46 -0500

    Add header links to the official site and the stream

--- src/static/App.svelte
+++ src/static/App.svelte
@@ -8,15 +8,27 @@
 		--sx-hue-gray: 180;
 	}
 	:global(header h1) {
-		font-size: var(--sx-font-size-6);
+		font-size: var(--sx-font-size-5);
 		font-family: "Press Start 2P", sans-serif;
 	}
 </style>
 
 <Header appName="GDQ Speedrun Reminder Bot">
-	<span class="sx-font-size-9" slot="logo">
+	<span class="sx-font-size-8" slot="logo">
 		<Icon icon="gamepad" />
 	</span>
+	<nav slot="headerEnd">
+		<ul>
+			{#each links as link}
+				<li>
+					<a href={link.href} target="_blank" rel="noreferrer noopener">
+						<Icon icon={link.icon} iconVariant={link.iconVariant} />
+						{link.text}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
 </Header>
 <main class="f-column f-1">
 	{#if !$eventStatus.initialized}
@@ -34,4 +46,19 @@
 	import { Header, Toasts, Icon, Loading } from "sheodox-ui";
 	import { eventStatus } from "./stores/schedule";
 	import Schedule from "./Schedule.svelte";
+
+	const links = [
+		{
+			text: "Twitch",
+			icon: "twitch",
+			iconVariant: "brand",
+			href: "https://www.twitch.tv/gamesdonequick",
+		},
+		{
+			text: "GDQ",
+			icon: "arrow-up-right-from-square",
+			iconVariant: "solid",
+			href: "https://gamesdonequick.com/",
+		},
+	] as const;
 </script>
